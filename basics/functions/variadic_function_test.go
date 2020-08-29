84fbff3033e01bb8cf3f435b50ea9ce6bbff999d
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-08-29 16:43:28 +1000

    format

--- basics/functions/variadic_function_test.go
+++ basics/functions/variadic_function_test.go
@@ -1,33 +1,29 @@
 package functions
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
-
 
 func PrtTestCase(args ...interface{}) {
 	fmt.Printf("expected: %v, got: %v, args: %v\n", args[0], args[1], args[2:])
 }
 
-
 func PrtTc(tc interface{}) {
 	fmt.Printf("%+v\n", tc)
 }
 
-
 func TestPrtTestCase(t *testing.T) {
 	var testCases = []struct {
-		value int
-		ss string
-		sub string
+		value    int
+		ss       string
+		sub      string
 		expected bool
-	} {
+	}{
 		{-1, "test", "testtest", false},
 	}
 	for _, tc := range testCases {
-		result := true
-		PrtTestCase(tc.expected, result, tc.value, tc.ss, tc.sub)
+		PrtTestCase(tc.expected, true, tc.value, tc.ss, tc.sub)
 		PrtTc(tc)
 	}
 }
