861c8f10169056692a8eadd80d2ac7bb9b15e363
Wei Ning (DA) <wei.ning@digitalasset.com>
2020-08-31 21:10:16 +1000

    wip

---
+++ algorithms/maps/map_test.go
@@ -0,0 +1,7 @@
+package maps
+
+import "testing"
+
+func TestCreateMap(t *testing.T) {
+
+}
--- algorithms/sequences/array_slice_test.go
+++ algorithms/sequences/array_slice_test.go
@@ -16,3 +16,26 @@ func length(xs []string) int {
 	xs[0] = "iddqd"
 	return len(xs)
 }
+
+func TestCreateSlice(t *testing.T) {
+	// allocate mem for a 4-elems array and create a slice for it, xs
+	xs := make([]string, 4)
+	xs[0] = "id"
+	// access to uninitialized element 1, 2, 3 - panic!
+	for i, w := range xs {
+		if i > 0 {
+			break
+		}
+		assert.Equal(t, "i", w[0:1])
+	}
+
+	ys := xs[:]
+	// both xs and ys hold references to the underlying array elements
+
+	// ns is a slice pointing a newly allocated array;
+	// copy() copies element-wise from xs to ns
+	ns := make([]string, 4)
+	copy(ns, xs)
+	assert.Equal(t, xs, ns)
+	assert.Equal(t, xs, ys)
+}
